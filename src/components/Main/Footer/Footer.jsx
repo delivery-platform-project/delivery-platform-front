f4a78d5984bb6af543aa3e4e2cfb0e1f676a3c2b
youngsik823 <youngsik823@naver.com>
2024-11-07 19:26:50 +0900

    feat: 메인 footer 제외 나머지 UI 구현 및 로그인 UI 구현

--- src/components/Main/Footer/Footer.jsx
+++ src/components/Main/Footer/Footer.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-    return <div className="footer-container">Footer</div>;
+    return (
+        <div className="footer-container">
+            <div className="footer-wrap">
+                <div className="footer-f">
+                    <div>
+                        <div>사업자정보확인</div>
+                        <div>이용약관</div>
+                        <div>전자금융거래이용약관</div>
+                        <div>개인정보처리방침</div>
+                    </div>
+                </div>
+                <div className="footer-s"></div>
+                <div className="footer-t"></div>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./Login.scss";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    return (
+        <div className="login-container">
+            <div className="login-wrap">
+                <div className="login-logo">
+                    <img src="/images/distribution.png" alt="" />
+                </div>
+                <div className="login-box">
+                    <div>
+                        <img className="login-icon" src="/images/user.png"></img>
+                        <input type="email" placeholder="Username" />
+                    </div>
+                    <div>
+                        <img className="login-icon" src="/images/password.png"></img>
+                        <input type="password" placeholder="Password" />
+                    </div>
+                </div>
+                <button className="login-btn">Login</button>
+                <p>Forgot your password?</p>
+                <button className="login-sign-up">SIGN UP</button>
+            </div>
+        </div>
+    );
+};
 
-export default Login
+export default Login;
