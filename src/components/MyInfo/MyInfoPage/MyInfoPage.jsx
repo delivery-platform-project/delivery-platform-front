b8050e712b42be95598081e47557f21732269c26
youngsik823 <youngsik823@naver.com>
2024-11-10 20:46:33 +0900

    feat: 내정보 UI UX 구현

---
+++ src/components/MyInfo/MyInfoPage/MyInfoPage.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./MyInfoPage.scss";
+
+const MyInfoPage = () => {
+    return (
+        <div className="my-info-page-container">
+            <div className="my-info-page-wrap">
+                <h1>내 정보 수정</h1>
+                <img className="my-info-page-profile-img" src="../../assert/user-icon.png"></img>
+                <div>
+                    <div className="my-info-page-name">닉네임</div>
+                    <input type="text" placeholder="먹자" />
+                </div>
+                <div>
+                    <div className="my-info-page-email">
+                        대표 이메일
+                        <input
+                            type="text"
+                            placeholder="youngsik823@naver.com"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <div className="my-info-page-phone">
+                        휴대폰 번호 변경
+                        <input type="text" placeholder="010-1234-5678" />
+                    </div>
+                </div>
+                <button className="my-info-page-btn">수정하기</button>
+                <div className="my-info-page-box-btn">
+                    <div className="my-info-page-logout">로그아웃</div>
+                    <div className="my-info-page-delete">회원탈퇴</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MyInfoPage;
--- src/pages/MyInfo/Myinfo.jsx
+++ src/pages/MyInfo/Myinfo.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./MyInfo.scss";
+import MyInfoPage from "../../components/MyInfo/MyInfoPage/MyInfoPage";
 
 const Myinfo = () => {
-    return <div>Myinfo</div>;
+    return (
+        <div className="my-Info-container">
+            <div className="my-Info-wrap">
+                <h1>내정보</h1>
+                <div className="my-Info-box">
+                    <img
+                        className="my-Info-profile"
+                        src="../../assert/user-icon.png"
+                    ></img>
+                    <div className="my-Info-box-wrap">
+                        <p>냠냠</p>
+                        <div className="my-Info-btn-box">
+                            <div className="my-Info-btn">리뷰관리</div>
+                            <div>주소관리</div>
+                        </div>
+                    </div>
+                </div>
+                <MyInfoPage/>
+            </div>
+        </div>
+    );
 };
 
 export default Myinfo;
