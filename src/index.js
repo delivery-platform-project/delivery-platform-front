f401e6023e57b572554ada7278d4783bbc411d1f
youngsik823 <youngsik823@naver.com>
2024-11-10 22:55:15 +0900

    add: 리액트 쿼리 설치

--- src/index.js
+++ src/index.js
@@ -6,13 +6,20 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
+
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+            {/* <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right"/> */}
+        </QueryClientProvider>
     </Provider>
 );
 
